9e0bc27dabc8c8d80e83033f67c7dfb83fc3bf99
Your Name <you@example.com>
2023-10-02 15:57:55 +0200

    10 bottles as goal

--- mobile/BLEApp2/GaugeChart.js
+++ mobile/BLEApp2/GaugeChart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Svg, Circle } from 'react-native-svg';
+import { Svg, Circle, Text as SvgText } from 'react-native-svg';
 
 export const GaugeChart = ({ value }) => {
   const radius = 80;
@@ -20,8 +20,19 @@ export const GaugeChart = ({ value }) => {
           strokeWidth={strokeWidth}
           strokeDasharray={dashArray}
           strokeLinecap="round"
-          transform={`rotate(90 ${radius} ${radius})`} // Rotate the gauge to start from the top
+          transform={`rotate(90 ${radius} ${radius})`}
         />
+        {value >= 10 && (
+          <SvgText
+            x={radius}
+            y={radius + 10}
+            textAnchor="middle"
+            fontSize="18"
+            fill="green" // You can adjust the color as needed
+          >
+            Success :)
+          </SvgText>
+        )}
       </Svg>
       <Text style={{ textAlign: 'center', fontSize: 18 }}>
         Bottles consumed: {value}
