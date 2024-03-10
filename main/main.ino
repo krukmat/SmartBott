ff8b6aeb2f5a4971dea6d504238b43381463f6da
Your Name <you@example.com>
2024-03-10 11:58:52 +0100

    testing 35cm height

--- main/main.ino
+++ main/main.ino
@@ -25,7 +25,7 @@ boolean counterEnabled = true;
 
 // bottle definitions
 const float radio = 7.5; // en cm
-const float altura_total = 40; // en cm
+const float altura_total = 35; // en cm
 const float capacidad_total = 7.5; // en litros
 
 // Define the bottle count and previous measure
