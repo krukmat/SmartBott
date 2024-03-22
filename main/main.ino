1a3795e704a7c2182ef50b22142c999601eda9ab
Your Name <you@example.com>
2024-03-22 08:14:27 +0100

    code cleanup

--- main/main.ino
+++ main/main.ino
@@ -11,17 +11,7 @@ BLEUnsignedIntCharacteristic bottleCharacteristic("19B10002-E8F2-537E-4F6C-D1047
 // Define the bottle types and sensor thresholds
 enum BottleType { LITRE_1_5, LITRE_0_5, LITRE_1, LITRE_8};
 BottleType bottleType = LITRE_8;
-const int THRESHOLD_FULL = 60;
-//const int THRESHOLD_HALF_1_5 = 190;
-//const int THRESHOLD_HALF_0_5 = 130;
-const int THRESHOLD_EMPTY_1_5 = 300;
-const int THRESHOLD_EMPTY_1 = 270;
-const int THRESHOLD_EMPTY_0_5 = 230;
 
-const int THRESHOLD_EMPTY_8 = 320;
-
-
-boolean counterEnabled = true;
 
 // bottle definitions
 const float radio = 3.5; // en cm
@@ -31,13 +21,8 @@ const float capacidad_total = 0.75; // en litros
 
 // Define the bottle count and previous measure
 int bottleCount = 0;
-enum Measure { EMPTY, HALF, FULL, UNAVAILABLE };
-Measure previousMeasure = EMPTY;
 const int ledPin = LED_BUILTIN; // D10; // pin to use for the LED
 
-//#define HIGH_SPEED
-//#define HIGH_ACCURACY
-//#define LONG_RANGE
 
 void setup() {
   Serial.begin(9600);
@@ -78,21 +63,6 @@ void setup() {
   sensor.startContinuous();
   sensor.setTimeout(500);
 
-  
-  #if defined LONG_RANGE
-    // lower the return signal rate limit (default is 0.25 MCPS)
-    sensor.setSignalRateLimit(0.1);
-    // increase laser pulse periods (defaults are 14 and 10 PCLKs)
-    sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-    sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
-  #endif
-  #if defined HIGH_SPEED
-    // reduce timing budget to 20 ms (default is about 33 ms)
-    sensor.setMeasurementTimingBudget(20000);
-  #elif defined HIGH_ACCURACY
-    // increase timing budget to 200 ms
-    sensor.setMeasurementTimingBudget(200000);
-  #endif  
 
   // Read Handler
   // Set up the read handler for the bottleCharacteristic
@@ -125,9 +95,6 @@ void setup() {
       } else if (datas[0] == 231) {
           bottleType = LITRE_1_5;
           Serial.println("1.5 litre");        
-      } else {
-        // toggle the bottle count init
-        counterEnabled = !counterEnabled;
       }
     } 
    Serial.print("+++++++++++++++++++++++++++++++++++++++++++++++++");
@@ -160,16 +127,7 @@ void loop() {
     Serial.println(reading);
     Serial.print("bottleType:");
     Serial.println(bottleType);
-  
-    if (bottleType != LITRE_8){
-      //updateBottleCount(reading);    
-      bottleCount = capacity(reading) * 100;
-    }
-    else {
-      // contar la cantidad de litros remanente
-      //countRemainingBottles(reading);
-      bottleCount = capacity(reading) * 100;
-    }
+    bottleCount = capacity(reading) * 100;
   }
 
   if (central) {
@@ -180,89 +138,14 @@ void loop() {
     // print the central's MAC address:
     Serial.println(central.address());
     if (central.connected()){
-        if ((bottleType != LITRE_8 && bottleCount > 0) || (bottleType == LITRE_8))
-          transmitBottleCount();
-      }
+        transmitBottleCount();
+    }
     digitalWrite(LED_BLUE, HIGH); 
   }
   delay(1000);
 }
 
-void countRemainingBottles(int reading) {
-  int remainingLitres = 0;
-  if (reading >= 340) {
-    bottleCount = 0;
-  } else if (reading >= 330 && reading < 340) {
-    bottleCount = 1;
-  } else if (reading >= 313 && reading < 330) {
-    bottleCount = 2;
-  } else if (reading >= 300 && reading < 313) {
-    bottleCount = 3;
-  } else if (reading >= 260 && reading < 300) {
-    bottleCount = 4;
-  } else if (reading >= 245 && reading < 260) {
-    bottleCount = 5;
-  } else if (reading >= 223 && reading < 245) {
-    bottleCount = 6;
-  } else if (reading >= 193 && reading < 223) {
-    bottleCount = 7;
-  } else if (reading >= 175 && reading < 193) {
-    bottleCount = 8;
-  } else if (reading >= 160 && reading < 175) {
-    bottleCount = 9;
-  } else if (reading >= 140 && reading < 160) {
-    bottleCount = 10;
-  } else if (reading >= 120 && reading < 140) {
-    bottleCount = 11;
-  } else if (reading >= 102 && reading < 120) {
-    bottleCount = 12;
-  } else if (reading >= 85 && reading < 102) {
-    bottleCount = 13;
-  } else if (reading >= 70 && reading < 85) {
-    bottleCount = 14;
-  } else {
-    bottleCount = 15;
-  }
-}
 
-
-void updateBottleCount(int reading) {
-  // Update the bottle count based on the sensor reading
-  Measure currentMeasure;
-  if (reading>1000 || reading < THRESHOLD_FULL){
-    currentMeasure = UNAVAILABLE;
-    counterEnabled = false;
-    return;
-  }
-  counterEnabled = true;
-
-  if (reading > 0 && reading <= THRESHOLD_FULL) {
-    currentMeasure = FULL;
-  } else if ( (bottleType == LITRE_1_5 && reading > THRESHOLD_FULL && reading < THRESHOLD_EMPTY_1_5) || 
-              (bottleType == LITRE_0_5 && reading > THRESHOLD_FULL && reading < THRESHOLD_EMPTY_0_5) ||  
-              (bottleType == LITRE_1 && reading > THRESHOLD_FULL && reading < THRESHOLD_EMPTY_1))  {
-    currentMeasure = HALF;
-  } else {
-    currentMeasure = EMPTY;
-  }
-  Serial.print("PreviousMeasure: ");
-  Serial.println(previousMeasure);
-  Serial.print("Current Measure: ");
-  Serial.println(currentMeasure);
-  Serial.print("Bottle count:");
-  Serial.println(bottleCount);
-  Serial.print("Counter enabled:");
-  Serial.println(counterEnabled);
-  if (counterEnabled && ((previousMeasure == HALF && currentMeasure == EMPTY) || (previousMeasure == FULL && currentMeasure == EMPTY))) {
-    bottleCount++;
-    //counterEnabled = false;
-    //bottleCount = 80;
-    Serial.println(bottleCount);
-  }
-  previousMeasure = currentMeasure;
-
-
-}
 
 void transmitBottleCount() {
   // Transmit the bottle count over BLE
