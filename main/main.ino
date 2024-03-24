563a19981af0a9e081b5f6972e82c6d6a8e11142
Your Name <you@example.com>
2024-03-24 16:09:26 +0100

    fixes overwritten

--- main/main.ino
+++ main/main.ino
@@ -5,22 +5,35 @@
 VL53L0X sensor;
 
 // Define the BLE service and characteristic
-BLEService bottleService("19B10001-E8F2-537E-4F6C-D104768A1214");
-BLEUnsignedIntCharacteristic bottleCharacteristic("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite);
+BLEService bottleService("FB6D03F2-9F80-411B-BE52-FA618EBFF138");
+BLEUnsignedIntCharacteristic bottleCharacteristic("FB6D03F3-9F80-411B-BE52-FA618EBFF138", BLERead | BLENotify | BLEWrite);
 
 // Define the bottle types and sensor thresholds
 enum BottleType { LITRE_1_5, LITRE_0_5, LITRE_1, LITRE_8};
 BottleType bottleType = LITRE_8;
+const int THRESHOLD_FULL = 60;
+//const int THRESHOLD_HALF_1_5 = 190;
+//const int THRESHOLD_HALF_0_5 = 130;
+const int THRESHOLD_EMPTY_1_5 = 300;
+const int THRESHOLD_EMPTY_1 = 270;
+const int THRESHOLD_EMPTY_0_5 = 230;
+
+const int THRESHOLD_EMPTY_8 = 320;
+
+
+boolean counterEnabled = true;
 
 // bottle definitions
-const float radio = 7.5; // en cm
-const float altura_total = 40; // en cm
+const float radio = 8; // en cm
+const float altura_total = 32; // en cm
+const float ajuste = 7;
 const float capacidad_total = 7.5; // en litros
 
 // Define the bottle count and previous measure
 int bottleCount = 0;
-const int ledPin = LED_BUILTIN; // D10; // pin to use for the LED
-
+enum Measure { EMPTY, HALF, FULL, UNAVAILABLE };
+Measure previousMeasure = EMPTY;
+const int ledPin = D10; // pin to use for the LED
 
 void setup() {
   Serial.begin(9600);
@@ -40,7 +53,7 @@ void setup() {
   }
 
   // set advertised local name and service UUID:
-  BLE.setLocalName("Hydro");
+  BLE.setLocalName("HydroTank");
   BLE.setAdvertisedService(bottleService);
 
   // add the characteristic to the service
@@ -61,7 +74,6 @@ void setup() {
   sensor.startContinuous();
   sensor.setTimeout(500);
 
-  
   // Read Handler
   // Set up the read handler for the bottleCharacteristic
   bottleCharacteristic.setEventHandler(BLEWritten, [](BLEDevice central, BLECharacteristic characteristic) {
@@ -93,9 +105,14 @@ void setup() {
       } else if (datas[0] == 231) {
           bottleType = LITRE_1_5;
           Serial.println("1.5 litre");        
-      } 
+      } else {
+        // toggle the bottle count init
+        counterEnabled = !counterEnabled;
+      }
     } 
    Serial.print("+++++++++++++++++++++++++++++++++++++++++++++++++");
+
+    // Include your logic here to handle the receivedValue
   });
 
   Serial.println("Hydro reader");
@@ -104,9 +121,10 @@ void setup() {
 float capacity(int distancia){
   // Convertir distancia a altura del líquido (en cm)
   float altura_liquido = altura_total - (distancia / 10.0 - ajuste); // convertir de mm a cm
-  
   Serial.println(altura_liquido);
-  
+  if (altura_liquido < 0){
+    altura_liquido = 0;
+  }
   // Calcular el volumen del líquido en el recipiente (en litros)
   float volumen_liquido = PI * pow(radio, 2) * altura_liquido / 1000.0;
   Serial.println(volumen_liquido);
@@ -123,26 +141,19 @@ void loop() {
     Serial.println(reading);
     Serial.print("bottleType:");
     Serial.println(bottleType);
-  
     bottleCount = capacity(reading) * 100;
-  }
-
+  } 
   if (central) {
-    pinMode(LED_BLUE, OUTPUT);
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(LED_BLUE, LOW); // changed from LOW to HIGH     
-    Serial.print("Connected to central: ");
-    // print the central's MAC address:
-    Serial.println(central.address());
-    if (central.connected()){
-        transmitBottleCount();
-    }
-    digitalWrite(LED_BLUE, HIGH); 
+     Serial.print("Connected to central: ");
+     // print the central's MAC address:
+     Serial.println(central.address());
+     if (central.connected()){
+         transmitBottleCount();
+     }
   }
+  
   delay(1000);
 }
-
-
 
 void transmitBottleCount() {
   // Transmit the bottle count over BLE
